6267ec27ae3f1f7d5d58ef4d173cd78fb8fff990
Nicholas <nschwenne@gmail.com>
2021-06-29 09:30:57 -1000

    commit tree 8f621549008fa3d61eab36799adb2fdd08fd1c41

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(cb) { 
+ cb();
+
+}
+
+
+function returnsANamedFunction() {
+    
+return function mathFunc() {
+
+}
+}
+
+function returnsAnAnonymousFunction() {
+return function () {
+    
+}
+}
